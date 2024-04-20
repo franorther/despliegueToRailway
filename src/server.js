6a039371b3eb0c620064b81d7d30ab1a3dc55b96
francisco.ortega <francisco.ortega@softlond.co>
2024-04-20 18:31:56 -0500

    port-added

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 require('dotenv').config();
-const { PORT_SERVER } = process.env
+const { PORT } = process.env
 
 app.use("/api", routes)
-app.listen(PORT_SERVER, () => {
-    console.log(`Servidor corriendo en puerto ${PORT_SERVER} ✨`);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT} ✨`);
 })
